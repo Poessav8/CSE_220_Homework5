1bf3c02f93e2ee0ea5702921ff3a544af96f32b9
Poessav8 <113644701+Poessav8@users.noreply.github.com>
2024-04-30 19:27:07 +0000

    first

--- hw5.asm
+++ hw5.asm
@@ -1,6 +1,20 @@
 .text
 
 init_student:
+	#$a0: student id 
+	#$a1: num_credits
+	#$a2: pointer to name 
+	#$a3: pointer to student record
+
+	#allocate memory for student record in heap
+	li $v0, 9 #srbk
+	li $a0, 8 #allocate 8 bytes for student record
+	syscall #$v0 points to memory 
+
+	sw $a0, 4($v0) #store sudent id
+	sw $a1, 2($v0) #store num_credits
+	sw $a2, 0($v0) #store pointer to student name
+
 	jr $ra
 	
 print_student:
